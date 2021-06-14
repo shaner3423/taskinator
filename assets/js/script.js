d4c8be5ad8392899429919dbee6d60f7a69208b1
Sugar Shane <scrisostomo17@gmail.com>
2021-06-13 23:28:29 -0400

    completed the add task button to add data input by customer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#save-task");
 var taskToDoEl = document.querySelector("#task-to-do");
 
-var createTaskHandler = function() {
-      //created a new task item
-      var listItemEl = document.createElement("li");
-      //style the new task item
-      listItemEl.className = "task-item";
-      //add the text
-      listItemEl.textContent = "This is a new task.";
-      //append this element to the task list
-      taskToDoEl.appendChild(listItemEl);
-}
+var createTaskHandler = function(event) {
+    //added a code that will prevent page from refreshing after filling out form
+    event.preventDefault(); 
 
-buttonEl.addEventListener("click", createTaskHandler);
+    //created a new task item
+    var listItemEl = document.createElement("li");
+    //style the new task item
+    listItemEl.className = "task-item";
+    //add the text
+    listItemEl.textContent = "This is a new task.";
+    //append this element to the task list
+    taskToDoEl.appendChild(listItemEl);
+};
 
+formEl.addEventListener("submit", createTaskHandler);
