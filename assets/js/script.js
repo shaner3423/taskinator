2e89bec25e8859524e49f3099348c3da1ba5811e
Sugar Shane <scrisostomo17@gmail.com>
2021-06-14 14:28:42 -0400

    completed refactoring code to organize to functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,41 @@
-var formEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#task-to-do");
 
-var createTaskHandler = function(event) {
-    //added a code that will prevent page from refreshing after filling out form
-    event.preventDefault(); 
+var taskFormHandler = function(event) {
+    //prevent default page behavior
+    event.preventDefault();
 
-    //created a new task item
-    var listItemEl = document.createElement("li");
-    //style the new task item
-    listItemEl.className = "task-item";
-    //add the text
-    listItemEl.textContent = "This is a new task.";
-    //append this element to the task list
-    taskToDoEl.appendChild(listItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+
+var createTaskEl = function(taskDataObj) {
+  //creat list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  //give it a class name 
+  taskInfoEl.className = "task-info";
+
+  //add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  //add entire list item to list
+  taskToDoEl.appendChild(listItemEl); 
+};
+
+formEl.addEventListener("submit", taskFormHandler);
